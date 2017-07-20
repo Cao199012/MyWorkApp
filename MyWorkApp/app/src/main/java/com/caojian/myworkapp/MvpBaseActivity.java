1941503852d1680fdeef3ae6feeae8f0da090f9f
CaoJian <695520214@qq.com>
2017-07-20 17:28:15 +0800

    mvp frame

---
+++ MyWorkApp/app/src/main/java/com/caojian/myworkapp/MvpBaseActivity.java
@@ -0,0 +1,33 @@
+package com.caojian.myworkapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CJ on 2017/7/20.
+ */
+
+public abstract class MvpBaseActivity<V,P extends BasePresenter<V>> extends AppCompatActivity{
+
+    protected P myPresenter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        myPresenter = createPresenter();
+        myPresenter.attachView((V) this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(myPresenter != null)
+        {
+            myPresenter.detachView();
+        }
+    }
+
+    public abstract P createPresenter();
+}
---
+++ MyWorkApp/app/src/main/java/com/caojian/myworkapp/mvp/MvpContract.java
@@ -0,0 +1,59 @@
+package com.caojian.myworkapp.mvp;
+
+/**
+ * Created by CJ on 2017/7/20.
+ */
+
+public class MvpContract {
+    /**
+     * M获取数据模块
+     */
+    public interface Model {
+        //请求数据
+        public void loadContent();
+    }
+
+    public interface View{
+        /**
+         * 隐藏加载界面
+         */
+        void disLoadingView();
+        //显示加载界面
+        void showLoadingView();
+        //显示错误界面
+        void showErrorView(String msg);
+        //显示数据刷新界面
+        void refreshContentView();
+        //显示加载更多界面
+        void loadMoreContentView();
+    }
+
+    /**
+     *
+     */
+    public interface Presenter{
+
+        /**
+         * 请求刷新的数据
+         */
+        void requestRefresh();
+
+        /**
+         * 请求加载更多的数据
+         */
+        void requestLoadMore();
+    }
+
+    /**
+     * @param <T>
+     *     presenter用于Model的回调借口
+     */
+    public interface InteractionListener<T>{
+        //请求成功
+        void onInteractionSuccess(T t);
+        //请求失败
+        void onInteractionFail(int errorCode, String errorMsg);
+    }
+
+
+}
